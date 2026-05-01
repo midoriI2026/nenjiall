1ab4190f1b0cc717f0693ca96c02c74c8dc67eb7
midoriI2026 <106957602+midoriI2026@users.noreply.github.com>
2026-05-01 10:41:00 +0900

    Remove match pattern from user script
    Removed the match pattern for the script.

--- nenjiall.user.js
+++ nenjiall.user.js
@@ -1,6 +1,5 @@
 // ==UserScript==
 // @name         一括読み込み
-// @match        https://midorinet-iwate.cybozu.com/o/*
 // @require      https://raw.githubusercontent.com/midoriI2026/nenji1/main/nenji1.js
 // @require      https://raw.githubusercontent.com/midoriI2026/nenji2/main/nenji2.js
 // ==/UserScript==
