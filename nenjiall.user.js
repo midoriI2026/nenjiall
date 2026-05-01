31985a5e9c141a5b286364e7142a9cb5be5a1ab0
midoriI2026 <106957602+midoriI2026@users.noreply.github.com>
2026-05-01 10:26:02 +0900

    Update nenjiall.user.js

--- nenjiall.user.js
+++ nenjiall.user.js
@@ -4,3 +4,17 @@
 // @require      https://raw.githubusercontent.com/midoriI2026/nenji1/main/nenji1.js
 // @require      https://raw.githubusercontent.com/midoriI2026/nenji2/main/nenji2.js
 // ==/UserScript==
+(function() {
+  'use strict';
+  const url = location.href;
+  const isDid35 = url.includes("did=35");
+  const isTargetPage =
+  url.includes("page=DBRecord") || url.includes("page=DBForm");  
+  if (isDid35 && isTargetPage) {
+    nenji1();
+  }
+  // DBRecord + did=35 のときだけ実行
+  if (url.includes("page=DBTaskForm") && url.includes("did=35")) {
+    nenji2();
+  }
+})();
